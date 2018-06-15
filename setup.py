9eb4c9d1971573cdd3b856477b437c0f446c6898
Peter O'Connor <peter.ed.oconnor@gmail.com>
2018-06-15 16:43:28 +0200

    added initial files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "spiking-equilibrium-prop",
+    author = "peter",
+    version = 0,
+    dependency_links = (),
+    install_requires = ['numpy', 'matplotlib'],
+    scripts = [],
+    packages=find_packages(),
+    )
